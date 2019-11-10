07565857426808a0142a5a65db2057fa20034cca
collinsbett29 <collinsbett29@gmail.com>
2019-11-10 22:16:00 +0300

    Test delete function

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        # create a new location and save
+        self.new_location = Location(name='Nairobi')
+        self.new_location.save()
+
+        # create a new category and save
+        self.new_category = Category(name='ocean')
+
+
