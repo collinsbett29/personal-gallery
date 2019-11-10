aed4cb7687517be76629ea007e89955b5b1f2314
collinsbett29 <collinsbett29@gmail.com>
2019-11-10 22:20:09 +0300

    Add vie function for categories

--- photos/views.py
+++ photos/views.py
@@ -1,35 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .models import Image
+
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
-    
+
+
+
+# FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
 def image_of_day(request):
     date = dt.date.today()
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> Photos of {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-
-
-
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
+    images = Image.todays_image()
+    return render(request, 'all-images/today-images.html', {"date": date, "images": images})
 
 
 # View Function to present images from past days
@@ -48,3 +33,47 @@ def past_days_image(request, past_date):
     images = Image.days_image(date)
     return render(request, 'all-images/past-images.html', {"date": date, "images": images})
 
+
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html',{"message":message})
+
+def search_result(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_location(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html',{"message":message})
+
+
+def image(request,image_id):
+    '''
+    This is a function that displays individual images
+    '''
+    try:
+        images = Image.objects.get(id=image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-images/image.html", {"image":images})
+
+
+def all_images(request):
+    images = Image.objects.all()
+
+    return render(request, "all-images/today-images.html", {"images": images})
