ef0ff3d3c9e154db10de7286c852d7cd9fa18895
collinsbett29 <collinsbett29@gmail.com>
2019-11-10 22:16:40 +0300

    Add image url

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.image_of_day,name='imagesToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImages')
-]
+    url(r'^images/',views.welcome,name = 'welcome'),
+    url(r'^$',views.all_images,name = 'image'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImages'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name = 'specificImage')   
+]    
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
