bdace0994c1a5d2a0ce829374d353ed7d4739d99
collinsbett29 <collinsbett29@gmail.com>
2019-11-11 16:00:59 +0300

    Run image class test

--- photos/tests.py
+++ photos/tests.py
@@ -4,17 +4,17 @@ from .models import Category,Location,Image
 # Create your tests here.
 class CategoryTestClass(TestCase):
     def setUp(self):
-        self.Party = Category(name='category').save()
+        self.category = Category(name='category').save()
 
     def test_new_category_isinstance_of_category(self):
-        self.assertTrue(isinstance(self.Party, Category))
+        self.assertTrue(isinstance(self.category, location))
 
 class LocationTestClass(TestCase):
     def setUp(self):
-        self.Nairobi = Location('location')
+        self.location = Location('location')
 
     def test_new_location_isinstance_of_location(self):
-        self.assertTrue(isinstance(self.Nairobi, Location))
+        self.assertTrue(isinstance(self.location, Location))
 
 
 class ImageTestClass(TestCase):
@@ -30,7 +30,7 @@ class ImageTestClass(TestCase):
 
         # Creating a new image and saving it
 
-        self.new_image= Image(name = 'Test Image',description = 'This is a random test image',category = 'Party', location='nairobi')
+        self.new_image= Image(name = 'Test Image',description = 'This is a random test image',category = 'category', location='location')
         self.new_image.save()
 
         self.new_image.category.location.add(self.new_category,self.new_location)
@@ -48,7 +48,7 @@ class ImageTestClass(TestCase):
         '''
         Test to confirm that we are getting images according to a given date
         '''
-        test_date = '2017-03-17'
+        test_date = '2019-11-09'
         date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
         image_by_date = Image.days_image(date)
         self.assertTrue(len(image_by_date) == 0)
