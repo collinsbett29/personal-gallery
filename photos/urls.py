4b99c1b6c491979ba97d6463bc4af18914ab7493
collinsbett29 <collinsbett29@gmail.com>
2019-11-11 10:04:57 +0300

    Remove archives url

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns=[
     url(r'^images/',views.welcome,name = 'welcome'),
     url(r'^$',views.all_images,name = 'image'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImages'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)',views.image,name = 'specificImage')   
 ]    
